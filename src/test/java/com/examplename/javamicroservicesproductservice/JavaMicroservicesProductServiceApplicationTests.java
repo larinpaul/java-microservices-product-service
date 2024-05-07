6aebca7ca8ba610cb8605b4cf91962cef615598c
larinpaul <www.larinpavel@mail.ru>
2024-05-07 07:20:30 +0500

    Created a MongoDB Container

--- src/test/java/com/examplename/javamicroservicesproductservice/JavaMicroservicesProductServiceApplicationTests.java
+++ src/test/java/com/examplename/javamicroservicesproductservice/JavaMicroservicesProductServiceApplicationTests.java
@@ -2,13 +2,26 @@ package com.examplename.javamicroservicesproductservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @Testcontainers
 class JavaMicroservicesProductServiceApplicationTests {
 
+	// The test will start the MongoDB Container, by downloading mongo:4.4.2 image
+	// And then it will add it to the spring.data.mongodb.uri dynamically
+	// at the time of creating the test
 
+
+	@Container
+	MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo.4.4.2");
+
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+	}
 
 	@Test
 	void contextLoads() {
